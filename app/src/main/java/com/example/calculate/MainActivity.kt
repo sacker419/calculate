90bbb6b228e7429e844885cff3980c3c4ae19c33
soldia <jiseong.r@gmail.com>
2024-02-01 17:12:04 +0900

    second commit function name ing

--- app/src/main/java/com/example/calculate/MainActivity.kt
+++ app/src/main/java/com/example/calculate/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.calculate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.example.calculate.databinding.ActivityMainBinding
+import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        binding.btnResult.setOnClickListener {
+
+        }
+    }
+
+    fun numClick(num: Int){
+
+    }
+
+    fun operClick(oper: String){
+
+    }
+
+    fun dotClick(){
+
+    }
+
+    fun equalClick(){
+
     }
 }
